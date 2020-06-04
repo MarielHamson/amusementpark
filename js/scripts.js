0e5da1c471efe25df9546dbd91a499dab068a0f8
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-04 11:43:40 -0700

    change logic to add highlighting to rides instead of showing / hiding sections
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
   let height = parseInt(prompt("How tall are you in cm?"));
 
   if (height >= 144.78 && height <= 195.65) {
-    $('#adult-rides').show();
+    $("#adult-rides").addClass("yellow-background");
   } else if (height <= 144.78) {
-    $('#kids-rides').show();
+    $("#kids-rides").addClass("pink-background");
   } else {
-    $('#tall').show();
-    $('#too-tall').show();
+    $("#tall").addClass("green-background");
   }
 });
