db30f1d79538766c3582e65bea0bb9ad346c8948
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-04 12:04:45 -0700

    change html file to add classes for the list to target specific rides to highlight
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   let height = parseInt(prompt("How tall are you in cm?"));
 
   if (height >= 144.78 && height <= 195.65) {
-    $("#adult-rides").addClass("yellow-background");
+    $("ul.adult").addClass("yellow-background");
   } else if (height <= 144.78) {
-    $("#kids-rides").addClass("pink-background");
+    $("ul.kids").addClass("pink-background");
   } else {
-    $("#tall").addClass("green-background");
+    $("ul.tall").addClass("green-background");
   }
 });
